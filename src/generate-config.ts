23bc09f9b0e22fcd758d4d0e1aadbe5e2500fb8b
AnilChinchawale <anil24593@gmail.com>
2023-07-21 15:27:36 +0530

    update dns records.

--- src/generate-config.ts
+++ src/generate-config.ts
@@ -19,10 +19,10 @@ const buildData = [
   {
     chainId: "51" as CHAIN_ID,
     documentStoreAddress: "0x268852277C0eED5A9999B41b0FdbA0443De76475",
-    tokenRegistryAddress: "0x27B8b1cBDb771653C987bF4B3918B5462dE25246",
-    dnsVerifiable: "distinct-harlequin-blackbird.sandbox.openattestation.com",
-    dnsTransferableRecord: "distinct-harlequin-blackbird.sandbox.openattestation.com",
-    dnsDid: "distinct-harlequin-blackbird.sandbox.openattestation.com",
+    tokenRegistryAddress: "0x1a378fEEc3ed9B63B872B11561FCf19f6d2CE793",
+    dnsVerifiable: "tradetrust-apothem.xdc.network	",
+    dnsTransferableRecord: "tradetrust-apothem.xdc.network	",
+    dnsDid: "tradetrust-apothem.xdc.network	",
   },
   {
     chainId: "80001" as CHAIN_ID,
